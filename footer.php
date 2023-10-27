4fc5e55fb935b2c608415c419ee137b62e830600
Joan Kenedy <joankenedi9311@gmail.com>
2023-10-27 16:05:41 -0600

    html-css

---
+++ footer.php
@@ -0,0 +1,145 @@
+<footer class="site-footer">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-lg-4 col-md-6 col-12 mb-3">
+                <div class="d-flex align-items-center mb-4">
+                    <img src="images/logo.png" class="img-fluid logo-image">
+
+                    <div class="d-flex flex-column">
+                        <strong class="logo-text">Multiservices</strong>
+                        <small class="logo-slogan">Bolsa de trabajo</small>
+                    </div>
+                </div>
+
+                <p class="mb-2">
+                    <i class="custom-icon bi-globe me-1"></i>
+
+                    <a href="#" class="site-footer-link">
+                        multiservicescard.com.mx
+                    </a>
+                </p>
+
+                <p class="mb-2">
+                    <i class="custom-icon bi-telephone me-1"></i>
+
+                    <a href="tel:3221328823" class="site-footer-link">
+                        3221328823
+                    </a>
+                </p>
+
+                <p>
+                    <i class="custom-icon bi-envelope me-1"></i>
+
+                    <a href="mailto:info@yourgmail.com" class="site-footer-link">
+                        infomultiservices@gmail.com
+                    </a>
+                </p>
+
+            </div>
+
+            <div class="col-lg-2 col-md-3 col-6 ms-lg-auto">
+                <h6 class="site-footer-title">Compañia</h6>
+
+                <ul class="footer-menu">
+                    <li class="footer-menu-item"><a href="#" class="footer-menu-link">Nosotros</a></li>
+
+
+
+                    <li class="footer-menu-item"><a href="#" class="footer-menu-link">Trabajos</a></li>
+
+                    <li class="footer-menu-item"><a href="#" class="footer-menu-link">Contacto</a></li>
+                </ul>
+            </div>
+
+            <div class="col-lg-2 col-md-3 col-6">
+                <h6 class="site-footer-title">Resources</h6>
+
+                <ul class="footer-menu">
+                    <li class="footer-menu-item"><a href="#" class="footer-menu-link">Guía</a></li>
+
+                    <li class="footer-menu-item"><a href="#" class="footer-menu-link">¿Cómo funciona?</a></li>
+
+                </ul>
+            </div>
+
+            <div class="col-lg-4 col-md-8 col-12 mt-3 mt-lg-0">
+                <h6 class="site-footer-title">Boletin informativo</h6>
+
+                <form class="custom-form newsletter-form" action="#" method="post" role="form">
+                    <h6 class="site-footer-title">Recibe notificaciones de noticias de empleo</h6>
+
+                    <div class="input-group">
+                        <span class="input-group-text" id="basic-addon1"><i class="bi-person"></i></span>
+
+                        <input type="text" name="newsletter-name" id="newsletter-name" class="form-control" placeholder="yourname@gmail.com" required>
+
+                        <button type="submit" class="form-control">
+                            <i class="bi-send"></i>
+                        </button>
+                    </div>
+                </form>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="site-footer-bottom">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-4 col-12 d-flex align-items-center">
+                    <p class="copyright-text">Copyright © MultiservicesCard 2023</p>
+
+                    <ul class="footer-menu d-flex">
+                        <li class="footer-menu-item"><a href="#" class="footer-menu-link">Política de privacidad</a></li>
+
+                        <li class="footer-menu-item"><a href="#" class="footer-menu-link">Términos</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-5 col-12 mt-2 mt-lg-0">
+                    <ul class="social-icon">
+                        <li class="social-icon-item">
+                            <a href="#" class="social-icon-link bi-twitter"></a>
+                        </li>
+
+                        <li class="social-icon-item">
+                            <a href="#" class="social-icon-link bi-facebook"></a>
+                        </li>
+
+                        <li class="social-icon-item">
+                            <a href="#" class="social-icon-link bi-linkedin"></a>
+                        </li>
+
+                        <li class="social-icon-item">
+                            <a href="#" class="social-icon-link bi-instagram"></a>
+                        </li>
+
+                        <li class="social-icon-item">
+                            <a href="#" class="social-icon-link bi-youtube"></a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-3 col-12 mt-2 d-flex align-items-center mt-lg-0">
+                    <p>Diseñado: <a class="sponsored-link" rel="sponsored" href="#" target="_blank">Agencia Inspiración</a></p>
+                </div>
+
+                <a class="back-top-icon bi-arrow-up smoothscroll d-flex justify-content-center align-items-center" href="#top"></a>
+
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- JAVASCRIPT FILES -->
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/counter.js"></script>
+<script src="js/custom.js"></script>
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,93 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Multiservices bolsa de trabajo</title>
+    <link rel="icon" type="image/png" href="images/logo.png" />
+
+    <!-- CSS FILES -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@100;300;400;600;700&display=swap" rel="stylesheet">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="css/bootstrap-icons.css" rel="stylesheet">
+
+    <link href="css/owl.carousel.min.css" rel="stylesheet">
+
+    <link href="css/owl.theme.default.min.css" rel="stylesheet">
+
+    <link href="css/tooplate-gotto-job.css" rel="stylesheet">
+
+    <!--
+
+Tooplate 2134 Gotto Job
+
+https://www.tooplate.com/view/2134-gotto-job
+
+Bootstrap 5 HTML CSS Template
+
+-->
+</head>
+
+<body id="top">
+
+    <nav class="navbar navbar-expand-lg">
+        <div class="container">
+            <a class="navbar-brand d-flex align-items-center" href="index.html">
+
+
+                <div class="d-flex flex-column">
+                    <strong class="logo-text">Multiservices</strong>
+                    <small class="logo-slogan">Bolsa de trabajo</small>
+                </div>
+            </a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav align-items-center ms-lg-5">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="index.php">Inicio</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.php">Nosotros</a>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">Más</a>
+
+                        <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarLightDropdownMenuLink">
+                            <li><a class="dropdown-item" href="job-listings.php">Listado de trabajos</a></li>
+
+                            <li><a class="dropdown-item" href="job-details.php">Detalles del trabajo</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.php">Contacto</a>
+                    </li>
+
+                    <li class="nav-item ms-lg-auto">
+                        <a class="nav-link" href="#">Registrate</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link custom-btn btn" href="#">Acceso</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
